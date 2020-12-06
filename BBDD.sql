78ba8702008073a18f2ba2e9872b5ddca26c4e33
jlmelchor <58067165+jlmelchor@users.noreply.github.com>
2020-12-06 19:40:20 +0100

    Doc. del proyecto final y fichero de carga de BD

---
+++ BBDD.sql
@@ -0,0 +1,272 @@
+-- FAMILY --
+
+INSERT INTO FAMILY VALUES (1, 'Ingrediente base');
+INSERT INTO FAMILY VALUES (2, 'Encurtidos');
+INSERT INTO FAMILY VALUES (3, 'Frutas');
+INSERT INTO FAMILY VALUES (4, 'Verduras');
+INSERT INTO FAMILY VALUES (5, 'Hortalizas');
+INSERT INTO FAMILY VALUES (6, 'Pastas y arroces');
+INSERT INTO FAMILY VALUES (7, 'Legumbres');
+INSERT INTO FAMILY VALUES (8, 'Frutos secos');
+INSERT INTO FAMILY VALUES (9, 'Carnes');
+INSERT INTO FAMILY VALUES (10, 'Pescados y mariscos');
+INSERT INTO FAMILY VALUES (11, 'Lácteos y huevos');
+INSERT INTO FAMILY VALUES (12, 'Especias');
+INSERT INTO FAMILY VALUES (13, 'Cereales y pan');
+INSERT INTO FAMILY VALUES (14, 'Dulces');
+
+
+-- INGREDIENT --
+
+INSERT INTO INGREDIENT VALUES (1, 'Aceite de coco', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (2, 'Aceite de girasol', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (3, 'Aceite de oliva', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (4, 'Aceitunas verdes', 'Y', 'Y', 'Y', 2);
+INSERT INTO INGREDIENT VALUES (5, 'Aceitunas negras', 'Y', 'Y', 'Y', 2);
+INSERT INTO INGREDIENT VALUES (6, 'Acelgas', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (7, 'Aguacate', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (8, 'Ajo', 'Y', 'Y', 'Y', 5);
+INSERT INTO INGREDIENT VALUES (9, 'Albahaca', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (10, 'Albaricoque', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (11, 'Alcachofa', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (12, 'Alitas de pollo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (13, 'Almendras', 'Y', 'Y', 'Y', 8);
+INSERT INTO INGREDIENT VALUES (14, 'Anchoas', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (15, 'Apio', 'Y', 'Y', 'Y', 5);
+INSERT INTO INGREDIENT VALUES (16, 'Arándanos', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (17, 'Atún', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (18, 'Avellanas', 'Y', 'Y', 'Y', 8);
+INSERT INTO INGREDIENT VALUES (19, 'Avena', 'Y', 'Y', 'Y', 13);
+INSERT INTO INGREDIENT VALUES (20, 'Azafrán', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (21, 'Azúcar', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (22, 'Bacalao', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (23, 'Bacon', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (24, 'Base de pizza', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (25, 'Batata', 'Y', 'Y', 'Y', 5);
+INSERT INTO INGREDIENT VALUES (26, 'Berenjena', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (27, 'Boquerones', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (28, 'Brócoli', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (29, 'Cacahuete', 'Y', 'Y', 'Y', 8);
+INSERT INTO INGREDIENT VALUES (30, 'Cacao', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (31, 'Café', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (32, 'Calabacín', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (33, 'Calabaza', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (34, 'Calabacín', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (35, 'Calamares', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (36, 'Caldo de pollo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (37, 'Canela', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (38, 'Carne de cerdo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (39, 'Carne de ternera', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (40, 'Cebolla', 'Y', 'Y', 'Y', 5);
+INSERT INTO INGREDIENT VALUES (41, 'Champiñones', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (42, 'Chocolate', 'Y', 'Y', 'Y', 14);
+INSERT INTO INGREDIENT VALUES (43, 'Chorizo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (44, 'Chuletas de cerdo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (45, 'Chuletón de ternera', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (46, 'Cilantro', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (47, 'Clavo', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (48, 'Coco', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (49, 'Comino', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (50, 'Carne de conejo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (51, 'Contramuslos de pollo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (52, 'Costillas de cerdo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (53, 'Curry', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (54, 'Dátiles', 'Y', 'Y', 'Y', 8);
+INSERT INTO INGREDIENT VALUES (55, 'Edulcorante', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (56, 'Eneldo', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (57, 'Entrecot de ternera', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (58, 'Esencia de vainilla', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (59, 'Espaguetis', 'Y', 'Y', 'Y', 6);
+INSERT INTO INGREDIENT VALUES (60, 'Espárragos', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (61, 'Espinacas', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (62, 'Fideos', 'Y', 'Y', 'Y', 6);
+INSERT INTO INGREDIENT VALUES (63, 'Filete de congrio', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (64, 'Filete de lenguado', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (65, 'Filete de merluza', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (66, 'Filete de pollo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (67, 'Filete de ternera', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (68, 'Frambuesas', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (69, 'Fresas', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (70, 'Galletas', 'Y', 'Y', 'Y', 14);
+INSERT INTO INGREDIENT VALUES (71, 'Gambas', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (72, 'Garbanzos', 'Y', 'Y', 'Y', 7);
+INSERT INTO INGREDIENT VALUES (73, 'Gelatina', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (74, 'Granada', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (75, 'Guisantes', 'Y', 'Y', 'Y', 7);
+INSERT INTO INGREDIENT VALUES (76, 'Harina', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (77, 'Higos', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (78, 'Huevo', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (79, 'Jamón cocido', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (80, 'Jamón serrano', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (81, 'Jengibre', 'Y', 'Y', 'Y', 5);
+INSERT INTO INGREDIENT VALUES (82, 'Judías blancas', 'Y', 'Y', 'Y', 7);
+INSERT INTO INGREDIENT VALUES (83, 'Judías verdes', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (84, 'Kiwi', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (85, 'Langostinos', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (86, 'Laurel', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (87, 'Leche', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (88, 'Leche condensada', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (89, 'Lechuga', 'Y', 'Y', 'Y', 5);
+INSERT INTO INGREDIENT VALUES (90, 'Lentejas', 'Y', 'Y', 'Y', 7);
+INSERT INTO INGREDIENT VALUES (91, 'Levadura', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (92, 'Limón', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (93, 'Maíz', 'Y', 'Y', 'Y', 13);
+INSERT INTO INGREDIENT VALUES (94, 'Mandarina', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (95, 'Mango', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (96, 'Mantequilla', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (97, 'Manzana', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (98, 'Masa de empanada', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (99, 'Mejillones', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (100, 'Mejillones', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (101, 'Melocotón', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (102, 'Merluza', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (103, 'Mermelada', 'Y', 'Y', 'Y', 14);
+INSERT INTO INGREDIENT VALUES (104, 'Miel', 'Y', 'Y', 'Y', 14);
+INSERT INTO INGREDIENT VALUES (105, 'Mozzarella', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (106, 'Muesli', 'Y', 'Y', 'Y', 13);
+INSERT INTO INGREDIENT VALUES (107, 'Muslos de pollo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (108, 'Naranja', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (109, 'Nata para montar', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (110, 'Nata para cocinar', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (111, 'Nueces', 'Y', 'Y', 'Y', 8);
+INSERT INTO INGREDIENT VALUES (112, 'Nuez moscada', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (113, 'Orégano', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (114, 'Pan', 'Y', 'Y', 'Y', 13);
+INSERT INTO INGREDIENT VALUES (115, 'Pan de molde', 'Y', 'Y', 'Y', 13);
+INSERT INTO INGREDIENT VALUES (116, 'Pan rallado', 'Y', 'Y', 'Y', 13);
+INSERT INTO INGREDIENT VALUES (117, 'Pasas', 'Y', 'Y', 'Y', 8);
+INSERT INTO INGREDIENT VALUES (118, 'Patata', 'Y', 'Y', 'Y', 5);
+INSERT INTO INGREDIENT VALUES (119, 'Pavo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (120, 'Pepinillos', 'Y', 'Y', 'Y', 2);
+INSERT INTO INGREDIENT VALUES (121, 'Pera', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (122, 'Perejil', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (123, 'Pimentón', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (124, 'Pimienta', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (125, 'Pimiento', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (126, 'Pistachos', 'Y', 'Y', 'Y', 8);
+INSERT INTO INGREDIENT VALUES (127, 'Piña', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (128, 'Piñones', 'Y', 'Y', 'Y', 8);
+INSERT INTO INGREDIENT VALUES (129, 'Plátano', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (130, 'Pollo', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (131, 'Puerro', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (132, 'Queso azul', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (133, 'Queso cheddar', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (134, 'Queso de cabra', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (135, 'Queso de oveja', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (136, 'Queso emmental', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (137, 'Queso crema', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (138, 'Queso gorgonzola', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (139, 'Queso parmesano', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (140, 'Rape', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (141, 'Repollo', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (142, 'Romero', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (143, 'Rúcula', 'Y', 'Y', 'Y', 4);
+INSERT INTO INGREDIENT VALUES (144, 'Sal', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (145, 'Salami', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (146, 'Salchichas', 'Y', 'Y', 'Y', 9);
+INSERT INTO INGREDIENT VALUES (147, 'Salmón', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (148, 'Salmón ahumado', 'Y', 'Y', 'Y', 10);
+INSERT INTO INGREDIENT VALUES (149, 'Sandía', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (150, 'Semillas de sésamo', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (151, 'Tomate', 'Y', 'Y', 'Y', 5);
+INSERT INTO INGREDIENT VALUES (152, 'Tomillo', 'Y', 'Y', 'Y', 12);
+INSERT INTO INGREDIENT VALUES (153, 'Uva', 'Y', 'Y', 'Y', 3);
+INSERT INTO INGREDIENT VALUES (154, 'Vinagre', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (155, 'Vino', 'Y', 'Y', 'Y', 1);
+INSERT INTO INGREDIENT VALUES (156, 'Yogur', 'Y', 'Y', 'Y', 11);
+INSERT INTO INGREDIENT VALUES (157, 'Zanahoria', 'Y', 'Y', 'Y', 5);
+
+
+-- Modificamos el nombre de las columnas porque habíamos insertado los datos en orden incorrecto --
+
+ALTER TABLE INGREDIENT RENAME COLUMN GLUTEN_FREE TO NAME1;
+ALTER TABLE INGREDIENT RENAME COLUMN NAME TO GLUTEN_FREE;
+ALTER TABLE INGREDIENT RENAME COLUMN NAME1 TO NAME;
+
+
+-- Indicamos los ingredientes veganos / vegetarianos / sin gluten --
+
+UPDATE INGREDIENT SET VEGAN = 'N' WHERE FAMILY_ID = 9;
+UPDATE INGREDIENT SET VEGAN = 'N' WHERE FAMILY_ID = 11;
+UPDATE INGREDIENT SET VEGAN = 'N' WHERE FAMILY_ID = 10;
+UPDATE INGREDIENT SET VEGAN = 'N' WHERE INGREDIENT_ID = 96;
+UPDATE INGREDIENT SET VEGETARIAN = 'N' WHERE FAMILY_ID = 9;
+UPDATE INGREDIENT SET VEGETARIAN = 'N' WHERE FAMILY_ID = 10;
+UPDATE INGREDIENT SET GLUTEN_FREE = 'N' WHERE FAMILY_ID = 6;
+UPDATE INGREDIENT SET GLUTEN_FREE = 'N' WHERE FAMILY_ID = 13;
+UPDATE INGREDIENT SET GLUTEN_FREE = 'N' WHERE INGREDIENT_ID = 24;
+UPDATE INGREDIENT SET GLUTEN_FREE = 'N' WHERE INGREDIENT_ID = 76;
+UPDATE INGREDIENT SET GLUTEN_FREE = 'N' WHERE INGREDIENT_ID = 98;
+
+
+--CATEGORY--
+
+INSERT INTO CATEGORY VALUES (1, 'A la plancha');
+INSERT INTO CATEGORY VALUES (2, 'Al horno');
+INSERT INTO CATEGORY VALUES (3, 'Carnes');
+INSERT INTO CATEGORY VALUES (4, 'Cócteles');
+INSERT INTO CATEGORY VALUES (5, 'Ensaladas');
+INSERT INTO CATEGORY VALUES (6, 'Entrantes');
+INSERT INTO CATEGORY VALUES (7, 'Mariscos y pescados');
+INSERT INTO CATEGORY VALUES (8, 'Pan');
+INSERT INTO CATEGORY VALUES (9, 'Pastas');
+INSERT INTO CATEGORY VALUES (10, 'Pizzas');
+INSERT INTO CATEGORY VALUES (11, 'Platos principales');
+INSERT INTO CATEGORY VALUES (12, 'Platos rápidos');
+INSERT INTO CATEGORY VALUES (13, 'Postres y dulces');
+INSERT INTO CATEGORY VALUES (14, 'Salsas');
+INSERT INTO CATEGORY VALUES (15, 'Sopas y cremas');
+INSERT INTO CATEGORY VALUES (16, 'Verduras');
+INSERT INTO CATEGORY VALUES (17, 'Zumos y batidos');
+
+
+-- RECIPE --
+
+INSERT INTO RECIPE VALUES (1, 'Estas lentejas a diferencia de la lentejas tradicionales no tienen carne y sí muchos vegetales. Introducimos más pimiento y un puerro que le da un sabor muy rico.
+
+Para ello picamos las cebollas, el ajo, el puerro y los pimientos en trozos muy pequeños para que se vayan deshaciendo en la cocción.
+Al final de la cocción casi no notaremos textura de ninguno de los ingredientes pero sí su sabor.
+Pelamos las zanahorias, las patatas y los tomates. Troceamos los tomates en 2 partes, las zanahorias las cortamos en rodajas finas y las patatas en pequeños trozos, así se harán mucho antes.
+En una cazuela, echamos un chorrito de aceite de oliva virgen extra, las cebollas, el puerro y los dientes de ajo. Sofreímos todo durante 10 minutos para que se mezclen bien los sabores.
+Añadimos los pimientos, las zanahorias en rodajas, los tomates y la hoja de laurel. Sofreímos también durante 2-3 minutos.
+Añadimos una cucharadita tipo postre de pimentón de la Vera dulce (podemos mezclar dulce y picante si os gusta un punto a las lentejas.
+Aquí también va por gustos, en casa nos gustan que sepan bastante a pimentón pues no lleva chorizo. Así que añadid a vuestro gusto.
+Removemos con una cuchara de madera rápidamente para que no se queme.
+Echamos las lentejas ya escurridas. Removemos otra vez para que se junte todo bien, unos 3 minutos.', 
+						   'Lentejas vegetales', 11);
+
+INSERT INTO RECIPE VALUES (2, 'Pelamos las patatas, las lavamos para quitar restos de suciedad y muy importante, las secamos.
+Cortamos en láminas semifinas, a mí no me gusta que se deshagan sino que al freírlas se tuesten un poco. Las colocamos en un bol grande, donde luego vamos a mezclar con el huevo y añadimos sal al gusto. Removemos bien y reservamos.
+Elegimos nuestra sartén más grande y antiadherente. La ponemos al fuego y añadimos un buen aceite de oliva virgen extra.
+No tengáis miedo en gastaros un poco de dinero en aceite, le va a dar ese punto de sabor que distingue vuestra tortilla de las demás, podéis emplear muchas variedades: arbequina, picual, cornicabra, hojiblanca, royal… el que más os guste, pero de calidad.
+Introducimos las patatas cortadas y ya saladas y dejamos que se cocinen durante aproximadamente veinte minutos a fuego bajo.
+El tema del grosor de las patatas también va a gustos. Hay quien prefiere cortarlas a trozos muy pequeños, en láminas muy finas que casi se rompan al freír y o más bien grandes.
+Mientras se están friendo las patatas, en el bol donde luego vamos a echar las patatas batimos los huevos, reservamos.
+Pelamos la cebolla y cortamos lo más fino posible.
+En otra sartén calentamos aceite de oliva y añadimos los trozos de cebolla.
+Pochamos hasta que tenga un color dorado, que tenga un punto de caramelización pero sin llegar a quemarse. La cebolla se hará antes que las patatas, así que escurrimos y añadimos al bol con el huevo batido.', 'Tortilla de patatas', 6);
+
+
+-- RECIPE_INGREDIENT --
+
+INSERT INTO RECIPE_INGREDIENT VALUES (1, 40, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (2, 3, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (3, 144, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (4, 8, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (5, 131, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (6, 125, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (7, 157, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (8, 118, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (9, 151, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (10, 86, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (11, 123, 1);
+INSERT INTO RECIPE_INGREDIENT VALUES (12, 90, 1);
+
+
+INSERT INTO RECIPE_INGREDIENT VALUES (13, 78, 2);
+INSERT INTO RECIPE_INGREDIENT VALUES (14, 118, 2);
+INSERT INTO RECIPE_INGREDIENT VALUES (15, 3, 2);
+INSERT INTO RECIPE_INGREDIENT VALUES (16, 40, 2);
+INSERT INTO RECIPE_INGREDIENT VALUES (17, 144, 2);
+
+
